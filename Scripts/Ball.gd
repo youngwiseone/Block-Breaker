e69aeb678bfff8ea908c661ecf6c985907a31f9f
bligh <bligh@katistuff.co.nz>
2016-06-24 17:57:08 +1200

    Updated, added Score, left click to create balls, and also added that you can change the direction the ball moves

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+export var speedup = 10
+const MAXSPEED = 400
+
 func _ready():
 	set_fixed_process(true)
 	
@@ -8,6 +11,16 @@ func _fixed_process(delta):
 	
 	for body in bodies:
 		if body.is_in_group("Bricks"):
+			get_node("/root/World").score += 5
 			body.queue_free()
+		
+		if body.get_name() == "Paddle":
+			var speed = get_linear_velocity().length()
+			var direction = get_pos() - body.get_node("Anchor").get_global_pos()
+			var velocity = direction.normalized()*min(speed+speedup, MAXSPEED)
+			set_linear_velocity(velocity)
+			
+	if get_pos().y > get_viewport_rect().end.y:
+		queue_free()
 
 
